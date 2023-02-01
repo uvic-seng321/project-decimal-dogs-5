842e0275f3fecc968edfbe33b221c6518fa6f0ed
Scott <skenning128@gmail.com>
2023-02-01 15:48:36 -0800

    firebase setup

--- website/src/App.js
+++ website/src/App.js
@@ -1,9 +1,23 @@
+import { BrowserRouter as Router, Route, Routes, Switch } from "react-router-dom";
 import { Header } from "./components/Header";
+import { logInWithEmailAndPassword } from "./firebase";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Reset from "./components/Reset";
+import Dashboard from "./components/Dashboard";
 
 function App() {
   return (
     <div className="bg-white">
-      <Header/>
+      {/* <Header/> */}
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Login />} />
+          <Route exact path="/register" element={<Register />} />
+          <Route exact path="/reset" element={<Reset />} />
+          <Route exact path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
