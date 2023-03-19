93b6d9ed13d00455cfeac80371a7a9df5ba03e0d
joe-mcdonald <joemcd0224@gmail.com>
2023-03-19 15:34:23 -0700

    Removed some dead code

--- tutorHub/server/tests/test_set_tutor_price.py
+++ tutorHub/server/tests/test_set_tutor_price.py
@@ -1,4 +1,4 @@
 from app import *
 def test_set_tutor_price():
-    response = app.test_client().get('/setTutorPrice/2/35.0')
+    app.test_client().get('/setTutorPrice/2/35.0')
     assert str(getTutorPrice(2)) == "35.0"
