49677d759003ed5fcf48fdc323f933763f056f3a
joe-mcdonald <joemcd0224@gmail.com>
2023-03-19 14:59:45 -0700

    Updated get price test to prevent overlap with set price test

--- tutorHub/server/tests/test_get_tutor_price.py
+++ tutorHub/server/tests/test_get_tutor_price.py
@@ -1,4 +1,4 @@
 from app import *
 def test_get_tutor_price():
     response = app.test_client().get('/getTutorPrice/1')
-    assert str(response.data.decode()) == "40.0"
+    assert str(response.data.decode()) == "30.0"
