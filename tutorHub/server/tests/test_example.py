e23f321cbffb31ba4d5e87552d20cca3c5398250
joe-mcdonald <joemcd0224@gmail.com>
2023-03-19 15:40:22 -0700

    Removed unnecessary comments.

--- tutorHub/server/tests/test_example.py
+++ tutorHub/server/tests/test_example.py
@@ -4,9 +4,3 @@ def test_test_route():
     response = app.test_client().get('/')
 
     assert response.data.decode() == 'Hello, World!'
-
-    # user story 8: tutor can advertise a precific price
-
-    #test idea: set a tutors price and the result of get price
-
-    #test idea: get current price, set to a new price, and the new price in the database != the old price
