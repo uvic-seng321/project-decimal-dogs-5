b5498f1ce5145ad77436ae120b20dbb8ec6d6462
muffin521 <60553778+muffin521@users.noreply.github.com>
2023-03-20 01:41:28 +0000

    add more failing tests

--- tutorHub/server/tests/test_new_users.py
+++ tutorHub/server/tests/test_new_users.py
@@ -6,3 +6,8 @@ def test_student_exists():
     stu_id = get_student_id("roman reigns")
     
     assert stu_id == 1
+
+def test_tutor_exists():
+    stu_id = get_tutor_id("Fred Murphy")
+    
+    assert stu_id == 1
