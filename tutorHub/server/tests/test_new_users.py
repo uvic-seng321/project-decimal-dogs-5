c730b604445d8d5a4ef7fe4fe40352460db8b7b8
muffin521 <60553778+muffin521@users.noreply.github.com>
2023-03-20 05:37:31 +0000

    add failing test to add users

--- tutorHub/server/tests/test_new_users.py
+++ tutorHub/server/tests/test_new_users.py
@@ -1,6 +1,6 @@
-import json
 from app import *
 from new_users import *
+from add_users import *
 
 def test_student_exists():
     stu_id = get_student_id("roman reigns")
@@ -8,6 +8,21 @@ def test_student_exists():
     assert stu_id == 1
 
 def test_tutor_exists():
-    stu_id = get_tutor_id("Fred Murphy")
+    tut_id = get_tutor_id("Fred Murphy")
     
-    assert stu_id == 1
+    assert tut_id == 1
+
+def test_add_student():
+    data_json = {
+        "id": 3,
+        "email": "test@test.com",
+        "name": "johnson smith",
+        "password": "password345"
+    }
+
+    app.test_client().post('/addStudent', json=data_json)
+    # result = app.test_client().get('/getAvailability/1')
+
+    stu_id = get_student_id("johnson smith")
+    
+    assert stu_id == 3
