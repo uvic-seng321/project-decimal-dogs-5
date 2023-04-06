c16537f34fc2b97a0597d5623f85a0a5b9a2ff79
muffin521 <60553778+muffin521@users.noreply.github.com>
2023-04-06 00:16:38 +0000

    fixing pylint errors

--- tutorHub/server/log_in.py
+++ tutorHub/server/log_in.py
@@ -1,19 +1,21 @@
+'''log in and sign up students'''
 from app import send_query
 
-# log in student
 def log_in_student(email, password):
-    query = f"SELECT * FROM Students WHERE email = '{email}' AND password = '{password};" 
+    '''log in students'''
+    query = f"SELECT * FROM Students WHERE email = '{email}' AND password = '{password};"
     student = send_query(query)
     if student == 0:
         # this is error case
         return "Incorrect email or password", 401
     else:
         return student
-        # login stuff here i can't redo this quickly sorry 
+        # login stuff here i can't redo this quickly sorry
 
 
 def register_student(email, name, password):
-    query = f"SELECT * FROM Students WHERE email = '{email}';" 
+    '''resister students'''
+    query = f"SELECT * FROM Students WHERE email = '{email}';"
     student = send_query(query)
     if student != 0:
         query = f"INSERT INTO Students (email, name, password) VALUES ('{email}', '{name}', '{password}');"
