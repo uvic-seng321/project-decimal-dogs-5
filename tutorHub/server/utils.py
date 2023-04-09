811bb14771cc3e3136cc6dc255becf08e7165334
Arun Alex <arunalex62@gmail.com>
2023-04-09 00:01:46 -0700

    Added utils.py file for send_query function.

---
+++ tutorHub/server/utils.py
@@ -0,0 +1,14 @@
+from db import get_db
+
+db = get_db()
+
+def send_query(query):
+    """Sends a query to the database and returns the result as a list of tuples"""
+    db.reconnect()
+    cur = db.cursor()
+    try:
+        cur.execute(query)
+        result = cur.fetchall()
+        return list(result)
+    except:
+        return 0
