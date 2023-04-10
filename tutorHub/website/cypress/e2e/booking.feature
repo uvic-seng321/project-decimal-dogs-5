35e3c8c189b1044b535198273dd14f6f05088aca
muffin521 <myfanwyw@uvic.ca>
2023-04-09 23:40:30 -0700

    broken but updated booking test

--- tutorHub/website/cypress/e2e/booking.feature
+++ tutorHub/website/cypress/e2e/booking.feature
@@ -1,5 +1,11 @@
 Feature: Creating a booking with a tutor
 
+  Scenario: Selecting a tutor to book with
+    Given I log in with "arufa@test.com" and "test12345"
+    When I visit the website "http://localhost:3000/tutors?subject=MATH&price=40&year=1"
+    When I click Book Tutor
+    Then I should be redirected to the tutors booking page
+
   Scenario: Creating a booking with a tutor
     Given I visit the website "http://localhost:3000/tutor/1"
     When I click book
--- tutorHub/website/cypress/e2e/booking.spec.ts
+++ tutorHub/website/cypress/e2e/booking.spec.ts
@@ -4,8 +4,29 @@ Given("I visit the website {string}", (url: string) => {
   cy.visit(url);
 });
 
+Given("I log in with {string} and {string}", (email: string, password: string) => {
+    cy.visit("http://localhost:3000/login");
+    cy.get('input[name="email"]').type(email);
+    cy.get('input[name="password"]').type(password);
+    cy.get("button[type='submit']").click();
+    cy.wait(2000);
+    cy.visit("http://localhost:3000/tutors?subject=MATH&price=40&year=1");
+});
+
+When("I visit the booking page", () => {
+    cy.url().should("include", "/tutors?subject=MATH&price=40&year=1");
+  });
+
+When("I click Book Tutor", () => {
+    cy.get('a[href="/tutor/1"]').contains('Book Tutor').click();
+});
+
+Then("I should be redirected to the tutors booking page", () => {
+  cy.url().should("include", "/tutor/1");
+});
+
+
 When("I click book", () => {
-    cy.wait(500);
     cy.get('button:contains("Book")').click();
 });
 
