906bbf0a7008cdd0364488a1fa884f06ec6830e3
joe-mcdonald <joemcd0224@gmail.com>
2023-04-09 23:19:20 -0700

    Added subjects to the tutor search screen

--- tutorHub/website/app/routes/index.tsx
+++ tutorHub/website/app/routes/index.tsx
@@ -3,6 +3,7 @@ import { json, redirect } from "@remix-run/node";
 import { getUser } from "~/utils/session.server";
 import { useState } from "react";
 import NavBar from "~/components/shared/Nav";
+import { subjects, years } from "~/constants";
 
 export const loader = async ({ request }: any) => {
   const user = await getUser(request);
@@ -24,16 +25,22 @@ export default function Index() {
       <div className="flex flex-col w-full h-full items-center justify-center font-bold">
         <div className="text-2xl font flex flex-col gap-y-4">
           <p className="">I need help with   
-            <select className="w-48 bg-gray-200 font-normal text-lg ml-2" onChange={() => setSubject("MATH")}>
-              <option value={1}>Subject</option>
-              <option value={2}>Subject</option>
+            <select className="w-48 bg-gray-200 font-normal text-lg ml-2" onChange={(e: any) => setSubject(e.target.value)}>
+              {subjects.map((subject) => {
+                return <option value={subject}>{subject}</option>
+              })
+            }
             </select>
           </p>
           <p className={`transition-all ease-in duration-500 ${subject ? "opacity-100 transalte-x-0" : "opacity-0 -translate-x-2"}`}>I am in my 
-            <select className="w-48 bg-gray-200 font-normal text-lg ml-2" onChange={() => setYear(1)}>
-              <option value={1}>Year</option>
-              <option value={2}>Year</option>
-            </select> year of classes
+            <select className="w-48 bg-gray-200 font-normal text-lg ml-2" onChange={(e: any) => setYear(e.target.value)}>
+              {years.map((year) => {
+                return <option value={year}>{year}</option>
+              })
+            }
+            </select>
+
+            
           </p>
           <p className={`transition-all ease-in duration-500 ${year ? "opacity-100 transalte-x-0" : "opacity-0 -translate-x-2"}`}>My budget is $
             <input 
